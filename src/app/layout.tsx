ec0ae3fd054e93b0311c5ca0d1548e4522725399
KaivDev4434 <kaivalyadixit@gmail.com>
2025-06-01 19:00:08 -0400

    Enable Vercel Speed Insights

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -31,6 +31,7 @@ export default function RootLayout({
           <main>{children}</main>
           <Footer />
         </ClientProviders>
+        <SpeedInsights />
       </body>
     </html>
   );
